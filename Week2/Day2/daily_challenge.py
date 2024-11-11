190204a1027316948bc7de7aa326cf3665af227e
Johnstake1 <jonathansvetskin@gmail.com>
2024-11-11 22:42:39 +0200

    Submitted

--- Week2/Day2/daily_challenge.py
+++ Week2/Day2/daily_challenge.py
@@ -6,8 +6,33 @@ class Pagination:
             items = []
         self.items = items
         self.pageSize = pagesize
+        self.currentPage = 1
+        self.totalPages = (len(self.items) + self.pageSize - 1) // self.pageSize
+
+
+    # def getVisibleItems(self):
+        # return self.items[self.items/self.pageSize] I dont know what is wrong here. maybe a float?
+    
+    def prevPage(self):
+        if self.currentPage > 1:
+            self.currentPage -= 1
+
+    def nextPage(self):
+        self.currentPage += 1
+
+    def firstPage(self):
+        self.currentPage = 1
+
+    def lastPage(self):
+        self.currentPage = self.lastPages
+    
+    # def goToPage(pageNum):
+
 
 alphabetList = list("abcdefghijklmnopqrstuvwxyz")
 
-p = Pagination(alphabetList, 4)
- 
+p = Pagination(alphabetList, 4)     
+
+# p.getVisibleItems()
+p.nextPage()
+     
