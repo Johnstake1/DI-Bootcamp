ddb34a1d47280f95d4b69d660fd296f09d36b0be
Johnstake1 <jonathansvetskin@gmail.com>
2024-11-17 14:16:04 +0200

    submission

---
+++ Week3/Day1/daily_challenge.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM actors
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+
+-- VALUES ('Matt', 'Damon', '10/08/1970', 5), ('George', 'Clooney', '05/06/1961', 2), ('Brad', 'Pitt', '12/18/1963', 4), 
+-- ('Angelina', 'Jolie', '06/04/1975', 3);
+-- DELETE FROM actors; Had many duplicates, therefore the actor_id will start from >6
+
+
+-- SELECT COUNT (*)
+-- FROM actors
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES ('Ricardo', 'Darin', '10/08/1940', 4)
