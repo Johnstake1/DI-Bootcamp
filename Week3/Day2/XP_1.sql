f70e314d9fd790f0c94e1fb4ff06f1445ac972a2
Johnstake1 <jonathansvetskin@gmail.com>
2024-11-18 16:04:27 +0200

    submissino of EX1-XP SQL

---
+++ Week3/Day2/XP_1.sql
@@ -0,0 +1,22 @@
+-- EX1
+
+-- SELECT * from items
+-- ORDER BY price ASC;
+
+-- SELECT items_name from items
+-- WHERE price >= 80
+-- ORDER BY price DESC;
+
+-- Select first_name, last_name from customers
+-- ORDER BY first_name
+-- LIMIT 3;
+
+-- Select last_name from customers
+-- Order by last_name DESC;
+
+
+
+
+
+
+
